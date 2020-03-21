2cc444d9eafef31b9c766a436b56af8ac65b7da3
SergiiPodlishchuk <podluy23@gmail.com>
2020-03-21 06:41:08 +0200

    hw-03

---
+++ homework-03/js/task-07.js
@@ -0,0 +1,79 @@
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw"
+};
+
+/*
+ * Каждая транзакция это объект со свойствами: id, type и amount
+ */
+
+const account = {
+  balance: 0,
+
+  // История транзакций
+  transactions: [],
+
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(amount, type) {
+    let deposit;
+    let withdraw;
+    let objTransaction;
+
+    if (type === deposit) {
+      objTransaction = {
+        id: "`id-${i}`",
+        amount: amount,
+        type: deposit
+      };
+    } else if (type === withdraw) {
+      objTransaction = {
+        id: "id-1",
+        amount: "amount",
+        type: withdraw
+      };
+    }
+  },
+
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {},
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {},
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {},
+
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {},
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {}
+};
+console.log(account.createTransaction(200, Transaction.DEPOSIT));
